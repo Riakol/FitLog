2f89b463f524578adc52a199577bdf60f7bb3ae7
Riakol <inspi47@gmail.com>
2025-12-26 19:25:29 +0500

    Register ExerciseRepository implementation in Hilt
    Created RepositoryModule in the data module to bind ExerciseRepositoryImpl to the ExerciseRepository interface using Dagger Hilt. Configured the repository as a singleton within the SingletonComponent.

---
+++ data/src/main/java/com/riakol/data/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.riakol.data.di
+
+import com.riakol.data.repository.ExerciseRepositoryImpl
+import com.riakol.domain.repository.ExerciseRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindExerciseRepository(
+        exerciseRepositoryImpl: ExerciseRepositoryImpl
+    ): ExerciseRepository
+}
